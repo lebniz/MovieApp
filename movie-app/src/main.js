caee3dbf7e212c173f4edcf3647617efcbc50b05
liu,yibei <lebniz@gmail.com>
2019-03-14 23:45:41 +0900

    update

--- movie-app/src/main.js
+++ movie-app/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+//import { store } from './store'
+import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+axios.defaults.baseURL = 'http://www.omdbapi.com/?apikey=d3a50d0d&page=1&Content-Type=application/json'
 new Vue({
   render: h => h(App),
+  //store,
+  router
 }).$mount('#app')
